304fb888d536080073e41a1de86c52df619595e5
spatil36 <spatil36@binghamton.edu>
2024-03-27 16:50:55 -0400

    Proj_3 commit

---
+++ proj_3/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstname="Bob" />
+            <Heading firstname="Sam" />
+        </div>
+    );
+};
+
+export default App;
---
+++ proj_3/src/Heading.js
@@ -0,0 +1,7 @@
+function Heading(props) {
+    return (
+        <h1>Hello, {props.firstname}</h1>
+    )
+}
+
+export default Heading;
